38fc8444904b95f1556867cb1916445b28cad75c
Dancan254 <dancanian25@gmail.com>
2025-05-29 11:50:28 +0300

    Fix the book service dependency injection issue

--- src/main/java/com/mwaisaka/Library/Management/System/LibraryManagementSystemApplication.java
+++ src/main/java/com/mwaisaka/Library/Management/System/LibraryManagementSystemApplication.java
@@ -1,11 +1,9 @@
 package com.mwaisaka.Library.Management.System;
 
-import org.modelmapper.Conditions;
-import org.modelmapper.ModelMapper;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+
 
 @SpringBootApplication
 public class LibraryManagementSystemApplication {
