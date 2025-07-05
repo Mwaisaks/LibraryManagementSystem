e780fd61580e26ab517cf67b157b29555473f561
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-07-05 11:54:13 +0300

    userResponse

--- src/main/java/com/mwaisaka/Library/Management/System/Dto/response/UserResponse.java
+++ src/main/java/com/mwaisaka/Library/Management/System/Dto/response/UserResponse.java
@@ -5,9 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-//@Data
-//@NoArgsConstructor
-//@AllArgsConstructor
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserResponse {
     private Long id;
     private String name;
