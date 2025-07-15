79e5618918056ba87fa003ffe2680c88ceee4508
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-07-15 18:00:18 +0300

    WIP: Update ApplicationConfig before pulling

--- src/main/java/com/mwaisaka/Library/Management/System/Config/ApplicationConfig.java
+++ src/main/java/com/mwaisaka/Library/Management/System/Config/ApplicationConfig.java
@@ -1,6 +1,5 @@
 package com.mwaisaka.Library.Management.System.Config;
 
-
 import org.modelmapper.Conditions;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
