2c40fbb9330d7171c11b58abde793f2314ee23c3
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-08-23 16:57:26 +0300

    Reservation repository

--- src/main/java/com/mwaisaka/Library/Management/System/service/impl/ReservationServiceImpl.java
+++ src/main/java/com/mwaisaka/Library/Management/System/service/impl/ReservationServiceImpl.java
@@ -1,4 +1,11 @@
 package com.mwaisaka.Library.Management.System.service.impl;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ReservationServiceImpl {
+
+    private final
 }
